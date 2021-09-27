3590083adc50149b9765b26722f6763387889477
Jakub <jakub.bchyla@gmail.com>
2021-09-27 17:20:15 +0200

    First commit

---
+++ Personal-Project-Menagment/src/main/java/com/Jakub686/Personal/Project/Menagment/PersonalProjectMenagmentApplication.java
@@ -0,0 +1,13 @@
+package com.Jakub686.Personal.Project.Menagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectMenagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectMenagmentApplication.class, args);
+	}
+
+}
